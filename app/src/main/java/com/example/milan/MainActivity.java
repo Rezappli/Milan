a1f2b413cbf70f2b1fa876d044cbf41dd4293c0d
Rezappli <66002646+Rezappli@users.noreply.github.com>
2020-11-15 22:40:10 +0100

    Login & Inscription V2

--- app/src/main/java/com/example/milan/MainActivity.java
+++ app/src/main/java/com/example/milan/MainActivity.java
@@ -9,15 +9,21 @@ import android.widget.Button;
 
 import com.example.milan.inscription.InscriptionActivity;
 import com.example.milan.inscription.NomUtilisateurFragment;
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonInscription;
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_ICON_ONLY);
+        signInButton.setColorScheme(SignInButton.COLOR_AUTO);
 
         initialiserWidjets();
     }
