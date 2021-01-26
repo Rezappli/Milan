5b3a23cabb3a31cbb55e6fa2e5a9840d0fd25da8
Thibault4 <thibault.boulay1@gmail.com>
2021-01-26 21:50:07 +0100

    Nav Header

--- app/src/main/java/com/example/milan/HomeActivity.java
+++ app/src/main/java/com/example/milan/HomeActivity.java
@@ -3,7 +3,9 @@ package com.example.milan;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
+import com.example.milan.utils.Constants;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -18,26 +20,40 @@ import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
+    //Constants.currentUser save current user connect to application
+
     private AppBarConfiguration mAppBarConfiguration;
+
+    private TextView navUsernameTextView;
+    private TextView navMailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        navUsernameTextView = headerView.findViewById(R.id.nav_header_username_textview);
+        navMailTextView = headerView.findViewById(R.id.nav_header_mail_textview);
+        navUsernameTextView.setText(Constants.currentUser.getUsername());
+        navMailTextView.setText(Constants.currentUser.getMail());
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_recherche, R.id.nav_messages, R.id.nav_amis, R.id.nav_video, R.id.nav_social, R.id.nav_room, R.id.nav_evenements)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/com/example/milan/utils/Constants.java
+++ app/src/main/java/com/example/milan/utils/Constants.java
@@ -1,9 +1,11 @@
 package com.example.milan.utils;
 
+import com.example.milan.objects.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Constants {
+    public static User currentUser;
     public static FirebaseFirestore mStoreBase = FirebaseFirestore.getInstance();
     public static FirebaseAuth mAuth = FirebaseAuth.getInstance();
 }
